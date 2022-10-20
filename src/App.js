6af7830fffdcd930048e59e36fa69ec3d56a4738
Bram <Bram.mathijssen@gmail.com>
2022-10-20 03:20:23 +0200

    search field to query the API for cocktails

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import "./App.css";
-import { useEffect, useRef, useState } from "react";
-import { computeHeadingLevel } from "@testing-library/react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [cocktails, setCocktails] = useState();
-  const [userInput, setUserInput] = useState("");
+  const [userInput, setUserInput] = useState("martini");
 
-  const fetchCocktails = async () => {
+  const fetchCocktails = async (search) => {
     const res = await fetch(
-      "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`
     );
 
     const data = await res.json();
     setCocktails(data);
+    console.log(data)
 
     return data;
   };
 
   useEffect(() => {
-    fetchCocktails();
-  }, []);
+    fetchCocktails(userInput);
+  }, [userInput]);
 
   const handleInput = (event) => {
     setUserInput(event.target.value);
@@ -28,21 +28,20 @@ function App() {
   return (
     <div className="App">
       <h1> Cocktails& Co </h1>
-      <input type="text" onChange={handleInput}></input>
+      <input type="text" defaultValue={userInput} onChange={handleInput}></input>
       {cocktails &&
         cocktails.drinks
-          .filter((drink) => {
-            console.log(
-              `in filter for ${drink.strDrink}, inputRef is ${userInput}`
-            );
-            return drink.strDrink.startsWith(userInput);
-          })
-          .map((drink, index) => {
+          // .filter((drink) => {
+          //   console.log(
+          //     `in filter for ${drink.strDrink}, inputRef is ${userInput}`
+          //   );
+          //   return drink.strDrink.startsWith(userInput);
+          // })
+          .map((drink) => {
             console.log(`in map for ${drink}`);
             return (
               <p key={drink.idDrink}>
-                {" "}
-                index: {index} strength: {drink.strDrink}
+                 drink: {drink.strDrink}
               </p>
             );
           })}
